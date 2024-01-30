778d688a53afdae1e13d4a366bf7e410d078a814
Pyae Phone Myat <pyaephonemyat2004@gmail.com>
2024-01-30 14:20:32 +0630

    fix UI

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -26,6 +26,24 @@ const Projects = () => {
         Projects
       </h1>
       <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-10'>
+        <div data-aos='fade-up'>
+          <a
+            href='https://bank-modern-web.vercel.app/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img
+              height={350}
+              width={250}
+              className={projectStyle}
+              src={hoobank}
+              alt='hoobank'
+            />
+            <p className='uppercase text-emerald-400 text-xl font-bold text-center mt-2'>
+              Modern Bank Web
+            </p>
+          </a>
+        </div>
         <div data-aos='fade-down'>
           <a
             href='https://pokemon-catch-theta.vercel.app/'
@@ -78,24 +96,6 @@ const Projects = () => {
             />
             <p className='uppercase text-emerald-400 text-xl font-bold text-center mt-2'>
               Dice Game
-            </p>
-          </a>
-        </div>
-        <div data-aos='fade-up'>
-          <a
-            href='https://bank-modern-web.vercel.app/'
-            target='_blank'
-            rel='noreferrer'
-          >
-            <img
-              height={350}
-              width={250}
-              className={projectStyle}
-              src={hoobank}
-              alt='hoobank'
-            />
-            <p className='uppercase text-emerald-400 text-xl font-bold text-center mt-2'>
-              Modern Bank Web
             </p>
           </a>
         </div>
